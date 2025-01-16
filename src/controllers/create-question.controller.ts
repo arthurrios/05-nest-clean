a8784baee6681054b1ebb0a0204ad0d925bb0d83
arthur <arthur.rios007@gmail.com>
2025-01-16 18:51:14 -0300

    feat: add jwt guard

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createQuestionBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type createQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle() {
+    return 'ok'
+  }
+}
