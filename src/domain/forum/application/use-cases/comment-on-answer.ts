b75e6f2b85f5fea3e1c8d09944ffa6a4e02be426
arthur <arthur.rios007@gmail.com>
2025-01-21 16:35:26 -0300

    feat: create comment on answer controller and e2e test

--- src/domain/forum/application/use-cases/comment-on-answer.ts
+++ src/domain/forum/application/use-cases/comment-on-answer.ts
@@ -1,9 +1,10 @@
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
-import type { AnswersRepository } from '../repositories/answers-repository'
+import { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
-import type { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
 import { left, right, type Either } from '@/core/either'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Injectable } from '@nestjs/common'
 
 interface CommentOnAnswerUseCaseRequest {
   authorId: string
@@ -16,6 +17,7 @@ type CommentOnAnswerUseCaseResponse = Either<
   { answerComment: AnswerComment }
 >
 
+@Injectable()
 export class CommentOnAnswerUseCase {
   constructor(
     private answersRepository: AnswersRepository,
